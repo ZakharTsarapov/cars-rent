62632da71ce06c62d50633f89c399ad91eab1ef6
ZakharTsarapov <tsarapovs@gmail.com>
2023-09-14 17:06:10 +0300

    fonts and loadmore

---
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -0,0 +1,8 @@
+import css from "./LoadMoreBtn.module.css";
+
+
+const LoadMoreBtn = ({onClick}) => {
+    return <button className={css.loadmore} type="button" onClick={onClick}>Load More</button>;
+  };
+  export default LoadMoreBtn;
+  
