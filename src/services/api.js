cd607c097576def394886d147625ebb8044f14b0
ZakharTsarapov <tsarapovs@gmail.com>
2023-09-14 11:13:24 +0300

    api

---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://649ade98bf7c145d0239a19c.mockapi.io';
+
+export const getAllCars = async () => {
+    const { data } = await axios.get('/Advert'); 
+    return data;
+  };
