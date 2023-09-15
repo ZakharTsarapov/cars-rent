38e07387c67e854549fcc56d5d264025eaf92673
ZakharTsarapov <tsarapovs@gmail.com>
2023-09-15 18:31:25 +0300

    constants

---
+++ src/constants/constants.js
@@ -0,0 +1,26 @@
+export const makes = [
+    "Buick",
+    "Volvo",
+    "HUMMER",
+    "Subaru",
+    "Mitsubishi",
+    "Nissan",
+    "Lincoln",
+    "GMC",
+    "Hyundai",
+    "MINI",
+    "Bentley",
+    "Mercedes-Benz",
+    "Aston Martin",
+    "Pontiac",
+    "Lamborghini",
+    "Audi",
+    "BMW",
+    "Chevrolet",
+    "Mercedes-Benz",
+    "Chrysler",
+    "Kia",
+    "Land"
+  ]
+
+export const price = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
