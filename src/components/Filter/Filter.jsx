55f204d5d8c90b7173d6b464f933643262164b14
ZakharTsarapov <tsarapovs@gmail.com>
2023-09-16 09:34:11 +0300

    style and Filter form

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,45 +1,60 @@
 import css from "./Filter.module.css"
 import { makes, price } from "constants/constants";
+import sprite from "../../images/sprite.svg"
 
 const Filter = () => {
     return (
         <form className={css.form}>
-        <div>
-            <label htmlFor="car-select">Car Brand</label>
-            <select name="cars" id="car-select" onChange={()=> {}}>
+        <div className={css.thumb}>
+            <label className={css.headLabel} htmlFor="car-select">Car Brand</label>
+            <select className={css.select} name="cars" id="car-select" onChange={()=> {}}>
                 <option value="without">All Cars</option>
                 {makes.map((el, index) => (
                     <option key={index} value={el}>{el}</option>
                 ))}
             </select>
+            <svg className={css.svg}>
+                <use width="20" height="20" href={`${sprite}#icon-chevron-down`} />
+            </svg>
         </div>
-        <div>
-            <label htmlFor="price-select">Price/ 1 hour</label>
-            <select name="price" id="price-select" onChange={()=> {}}>
+        <div className={css.thumb}>
+            <label className={css.headLabel} htmlFor="price-select">Price/ 1 hour</label>
+            <select className={css.secondSelect} name="price" id="price-select" onChange={()=> {}}>
                 <option value="without">To $</option>
                 {price.map((el, index) => (
                     <option key={index} value={el}>{el}</option>
                 ))}
             </select>
+            <svg className={css.svg}>
+                <use width="20" height="20" href={`${sprite}#icon-chevron-down`} />
+            </svg>
         </div>
+        <div className={css.thumb}>
+        <label className={css.headLabel}>Car mileage / km</label>
         <div className={css.inputs}>
-            <div>
+            <div className={css.inputThumb}>
                 <input
+                className={css.input}
                 type="text"
                 name="mileageFrom"
-                id="mileageFrom">   
+                id="mileageFrom"
+                style={{ paddingLeft: '60px', borderRadius: '14px 0 0 14px' }}>   
                 </input>
-                <label htmlFor="mileageTo">From</label>
+                <label className={css.inpText} htmlFor="mileageTo">From</label>
             </div>
-            <div>
+            <div className={css.inputThumb}>
                 <input
+                className={css.input}
                 type="text"
                 name="mileageTo"
-                id="mileageTo">
+                id="mileageTo"
+                style={{ paddingLeft: '44px', borderRadius: '14px 0 0 14px' }}>
                 </input>
-                <label htmlFor="mileageTo">To</label>
+                <label className={css.inpText} htmlFor="mileageTo">To</label>
             </div>
         </div>
+        </div>
+
         <button className={css.btn} type="submit" onClick={()=> {}}>Search</button>
         </form>
     )
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,6 +3,7 @@ import css from "./Home.module.css"
 const Home = () => {
   return (
     <div className={css.container}>
+    <h1 className={css.hero}>"Drive our own Dream" - Zakhar T.</h1>
     </div>
   );
 };
