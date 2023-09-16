32bbb0a19f9bc65af7ccb3a54cc3de647de3c4e1
ZakharTsarapov <tsarapovs@gmail.com>
2023-09-16 11:56:16 +0300

    filter

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -1,26 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-    filterCars: "",
-    filterPrice: "",
-    filterMileage: {
-        from:"",
-        to:""
-    }
-}
+  filterCars: '',
+  filterPrice: '',
+  filterMileage: {
+    from: '',
+    to: '',
+  },
+};
 
 const filterSlice = createSlice({
-    name: filter,
-    initialState,
-    reducers: {
-        updateFilter(state, {payload}) {
-            state.filterCars = payload.cars,
-            state.filterPrice = payload.price,
-            state.filterMileage.from = payload.mileageFrom,
-            state.filterMileage.to = payload.mileageTo
-        }
-    }
-})
+  name: 'filter',
+  initialState,
+  reducers: {
+    updateFilter(state, { payload }) {
+      (state.filterCars = payload.cars)
+        (state.filterPrice = payload.price)
+        (state.filterMileage.from = payload.mileageFrom)
+        (state.filterMileage.to = payload.mileageTo)
+    },
+  },
+});
 
 export const filterReducer = filterSlice.reducer;
-export const updateFilter = filterSlice.actions;
+export const { updateFilter } = filterSlice.actions;
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,4 +1,27 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectCars = state => state.cars.items;
 export const selectIsLoading = state => state.cars.isLoading;
 export const selectError = state => state.cars.error;
 export const selectFavorites = state => state.cars.favorites;
+export const selectFilterCars = state => state.filter.filterCars;
+export const selectFilterPrice = state => state.filter.filterPrice;
+export const selectFilterMileageFrom = state => state.filter.filterMileage.from;
+export const selectFilterMileageTo = state => state.filter.filterMileage.to;
+
+export const selectByCar = createSelector(
+  [selectFilterCars, selectCars],
+  (filterCars, cars) => {
+    const normalizeFilterCars = filterCars.toLowerCase();
+    return cars.filter((car) =>
+      car.make.toLowerCase().includes(normalizeFilterCars)
+    );
+  }
+);
+
+export const selectByPrice = createSelector(
+    [selectFilterPrice, selectCars],
+    (filterPrice, cars) => {
+        return cars.filter((car) => car.rentalPrice <= filterPrice)
+    }
+)
